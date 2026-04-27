3193bc8121400da5adf59c45f0c1a5f7a3cf07f1
menurhosain <omithasan005@gmail.com>
2026-04-27 12:20:28 +0600

    added: nodemailer configaretion for email service

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,22 @@
 module.exports = () => ({
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: process.env.SMTP_HOST,
+        port: parseInt(process.env.SMTP_PORT, 10),
+        secure: process.env.SMTP_SECURE === "true",
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS,
+        },
+      },
+      settings: {
+        defaultFrom: process.env.SMTP_FROM,
+        defaultReplyTo: process.env.SMTP_FROM,
+      },
+    },
+  },
   upload: {
     config: {
       sizeLimit: 5 * 1024 * 1024, //5mb
