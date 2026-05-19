ac740bab5bc2c0d7c249ee63285a807e5d9d4344
menurhosain <omithasan005@gmail.com>
2026-05-19 14:40:39 +0600

    create new service-details type

---
+++ src/api/service-details/services/service-details.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * service-details service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::service-details.service-details');
