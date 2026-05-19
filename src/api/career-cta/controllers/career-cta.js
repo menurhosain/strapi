a7029b2a4faf47149a9fd6b4547f8672405003e5
rafiansagar <sagarworld420@gmail.com>
2026-05-19 10:22:09 +0600

    carrer cta component added

---
+++ src/api/career-cta/controllers/career-cta.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * career-cta controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::career-cta.career-cta');
---
+++ src/api/career-cta/routes/career-cta.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * career-cta router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::career-cta.career-cta');
