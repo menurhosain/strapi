db2dad1549cc5472b1bff0275b68e06ba834fce2
rafiansagar <sagarworld420@gmail.com>
2026-05-20 10:39:47 +0600

    partner testimonial api set to voice

---
+++ src/api/voices-of-experience/controllers/voices-of-experience.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::voices-of-experience.voices-of-experience');
---
+++ src/api/voices-of-experience/routes/voices-of-experience.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::voices-of-experience.voices-of-experience');
