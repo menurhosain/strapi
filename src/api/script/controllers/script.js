2fe0fe9ba962038ef8cccc6ee5b192e5c2788ce3
menurhosain <omithasan005@gmail.com>
2026-06-09 12:09:57 +0600

    removed all collection type except contractor,subcontracted,location & single collection

--- src/api/script/controllers/script.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * script controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::script.script');
--- src/api/script/services/script.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * script service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::script.script');
